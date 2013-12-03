90482126a5bd77c1f289dbcd697d5fcfbadf6794
dnatsvlishvili <d.natsvlishvili@gmail.com>
2013-12-03 05:02:42 +0400

    Adding a translate method

--- geo-key.js
+++ geo-key.js
@@ -35,29 +35,31 @@
 (function(window, document, undefined){
   "use strict";
   var GeoKey = function(params){
+    var defaults = {
+      target: ''
+    };
     
     // Provides ability to provide custom parameters
-    var params = function(source) {
-      var target = {target: ''};
-      if (source) {
-        for (var key in source) {
-          var val = source[key];
-          if (typeof val !== "undefined") {
-            target[key] = val;
+    this.params = function(input, defaults) {
+      if (input) {
+        for (var key in input) {
+          var val = input[key];
+          if (typeof val !== 'undefined') {
+            defaults[key] = val;
           }
         }
       }
-      return target;
-    }(params);
+      return defaults;
+    }(params, defaults);
     
     // Elements that need to be worked on
-    this.elements = function(){
+    this.elements = function(params){
       if (params.target === '') {
         return document.querySelectorAll('input[type=text],input[type=edit]');
       } else {
         return document.getElementsByClassName(params.target);
       }
-    }();
+    }(this.params);
     
     /* Inline test case */
     for (var i in this.elements) {
@@ -65,20 +67,81 @@
     }
   };
   
-  // HTML5 Polyfills
-  if (!document.querySelectorAll){
-    document.querySelectorAll = function(selector){
-      var doc = document,
-      head = doc.documentElement.firstChild,
-      styleTag = doc.createElement('STYLE');
-      head.appendChild(styleTag);
-      doc.__qsaels = [];
-
-      styleTag.styleSheet.cssText = selector + "{x:expression(document.__qsaels.push(this))}";
-      window.scrollBy(0, 0);
-
-      return doc.__qsaels;
+  GeoKey.prototype.translate = function(string) {
+    var input = string.split('');
+    var result = '', offset, chr;
+    
+    var charset = {
+      a: 1, b: 1, d: 1, e: 1, i: 1, g: -3, v: -15,
+      z: -18, T: 21, J: 39, f: 16, q: 6, R: 38, S: 39,
+      C: 56, c: 25, Z: 35, w: 7, W: 40, x: 8, j: 23, h: 26
+    };
+    
+    var scope = [67, 74, 82, 83, 84, 87, 90];
+    for (var n = 96; n < 122; n += 1, scope.push(n));
+    
+    for (var i = 0; i < input.length; i += 1) {
+      chr = input[i];
+      
+      offset = chr.charCodeAt();
+      if (typeof charset[chr] !== 'undefined') {
+        offset += charset[chr];
+      }
+      
+      result += (scope.indexOf(chr.charCodeAt()) > -1) ? String.fromCharCode(4206 + offset) : chr;
     }
+    
+    return result;
+  };
+  
+  // HTML5 Polyfills
+  if (!document.querySelectorAll) {
+    document.querySelectorAll = function (selectors) {
+      var style = document.createElement('style'), elements = [], element;
+      document.documentElement.firstChild.appendChild(style);
+      document._qsa = [];
+
+      style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
+      window.scrollBy(0, 0);
+      style.parentNode.removeChild(style);
+
+      while (document._qsa.length) {
+        element = document._qsa.shift();
+        element.style.removeAttribute('x-qsa');
+        elements.push(element);
+      }
+      document._qsa = null;
+      return elements;
+    };
+  }
+
+  if (!document.getElementsByClassName) {
+    document.getElementsByClassName = function (classNames) {
+      classNames = String(classNames).replace(/^|\s+/g, '.');
+      return document.querySelectorAll(classNames);
+    };
+  }
+  
+  if (!Array.prototype.map) {
+    Array.prototype.map = function (fun /*, thisp */) {
+      "use strict";
+
+      if (this === void 0 || this === null) { throw new TypeError(); }
+
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (typeof fun !== "function") { throw new TypeError(); }
+
+      var res = []; res.length = len;
+      var thisp = arguments[1], i;
+      for (i = 0; i < len; i++) {
+        if (i in t) {
+          res[i] = fun.call(thisp, t[i], i, t);
+        }
+      }
+
+      return res;
+    };
   }
   
   window.GeoKey = GeoKey;
